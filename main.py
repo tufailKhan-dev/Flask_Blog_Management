f89d995395fa88aa4578b71b2a51d2c8437980f0
Dev <downlaodsnowinx2speed.com>
2024-04-18 19:39:05 +0530

    database error solve

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ from flask import Flask,render_template, flash
 from flask_wtf import FlaskForm
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from wtforms import SelectField, SubmitField, StringField
+from wtforms import SubmitField, StringField
 from wtforms.validators import DataRequired
 # create instance
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "my name is Batman"
 # add data base
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite'
+app.config['SQLALCHEMY_TRANK_MODIFICATIONS'] = False
 #initial lize database
 db = SQLAlchemy(app)
-
+app.app_context().push()
 #create a model
 class Users(db.Model):
     id= db.Column(db.Integer, primary_key=True)
@@ -50,9 +51,26 @@ class NameerForm(FlaskForm):
 
 @app.route('/user/add', methods=["GET","POST"])
 def adduser():
+    name = None
     form_obj = UserForm()
+    if form_obj.validate_on_submit():
+        user = db.session.query(Users.query.filter_by(email=form_obj.email.data).first())
+        if user is None:    
+            user = Users(name=form_obj.name.data, email=form_obj.email.data)
+            db.session.add(user)
+            db.session.commit()
+        name = form_obj.name.data
+        form_obj.name.data = ''
+        form_obj.email.data = ''
+        flash("User Added Successfully!")
+    AllUsers = Users.query.order_by(Users.date_added)
+    return render_template("adduser.html",
+                           form_obj=form_obj,
+                            name = name,
+                            AllUsers = AllUsers
+                            )
 
-    return render_template("adduser.html", form_obj=form_obj)
+
 
 @app.route('/')
 def main():
@@ -86,6 +104,7 @@ def form():
         form_obj.name.data = ''
         flash('Successful submit form', 'success')
 
-    return render_template('form.html', 
-                           name=name,
-                           form_obj = form_obj)
+    return render_template('form.html', name=name, form_obj=form_obj)
+
+
+
